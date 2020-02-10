e513b641c9e36ac24d5a23c724f2747c980448c1
EricLi <eric2927.li@gmail.com>
2020-02-10 11:11:41 -0500

    In class

---
+++ InClassExercise/src/SampleClass.cpp
@@ -0,0 +1,18 @@
+/*
+ * SampleClass.cpp
+ *
+ *  Created on: Feb 10, 2020
+ *      Author: eric_li
+ */
+
+#include "SampleClass.h"
+
+SampleClass::SampleClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SampleClass::~SampleClass() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ InClassExercise/src/SampleClass.h
@@ -0,0 +1,20 @@
+/*
+ * SampleClass.h
+ *
+ *  Created on: Feb 10, 2020
+ *      Author: eric_li
+ */
+
+#ifndef SAMPLECLASS_H_
+#define SAMPLECLASS_H_
+
+class SampleClass {
+public:
+	SampleClass();
+	virtual ~SampleClass();
+
+private:
+	int privAttribute;
+};
+
+#endif /* SAMPLECLASS_H_ */
